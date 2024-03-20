ea23e5706f80cc0eb56c7cee74fb33dd588b9b80
Ibo <contact@iboibelings.nl>
2024-03-20 13:41:12 +0100

    Fix plotter reset and movement commands

--- resetPlotter.py
+++ resetPlotter.py
@@ -3,16 +3,23 @@ import time
 import random
 
 s = serial.Serial('/dev/ttyUSB0', 115200)
-s.write("\r\n\r\n".encode())
+
+
+def toPlotter(command):
+  s.write(str(command + '\n').encode())
+
+
+# s.write("\r\n\r\n".encode())
+toPlotter("\r\n\r")
 time.sleep(2)
 s.flushInput()
 
-s.write(str('$h\n').encode())
-s.write(str('$1=255\n').encode())
+toPlotter('$h')
+toPlotter('$1=255')
 time.sleep(1)
-s.write(str('G92x0y0\n').encode())
-s.write(str('g1f1000\n').encode())
-s.write(str('z5\n').encode())
+toPlotter('G92x0y0')
+toPlotter('g1f1000')
+toPlotter('z5')
 
 time.sleep(2)
 
@@ -20,18 +27,18 @@ counter = 0
 
 print("start moving")
 
-while counter < 10:
-  #while True:
-  randomX = random.random() * 50
-  randomY = random.random() * 200
-  randomSpeed = (random.random() * 1000) + 100
-  coord = "x{0}y{1}f5000\n".format(randomX, randomY)
-  coord = str(coord)
-  s.write(coord.encode())
-  time.sleep(3)
+# while counter < 10:
+#   #while True:
+#   randomX = random.random() * 50
+#   randomY = random.random() * 200
+#   randomSpeed = (random.random() * 1000) + 100
+#   coord = "x{0}y{1}f5000\n".format(randomX, randomY)
+#   coord = str(coord)
+#   s.write(coord.encode())
+#   time.sleep(3)
 
-  counter = counter + 1
-  print(counter)
+#   counter = counter + 1
+#   print(counter)
 
 
 s.write(str('z0f1000\n').encode())
