288e99f33815052e5a26bfd82b7d353e7bcdeb5a
Ibo <contact@iboibelings.nl>
2024-03-20 13:59:09 +0100

    Refactor manualControl.py to remove unnecessary code and add input prompt

--- manualControl.py
+++ manualControl.py
@@ -4,18 +4,20 @@ import keyboard
 
 resetSequence()
 
-while True:
-  try:
-    key = keyboard.read_key()
-    if key == 'up':
-      print('up')
-    elif key == 'down':
-      print('down')
-    elif key == 'left':
-      print('left')
-    elif key == 'right':
-      print('right')
-    else:
-      print('Invalid key')
-  except KeyboardInterrupt:
-    break
+input()
+
+# while True:
+#   try:
+#     key = keyboard.read_key()
+#     if key == 'up':
+#       print('up')
+#     elif key == 'down':
+#       print('down')
+#     elif key == 'left':
+#       print('left')
+#     elif key == 'right':
+#       print('right')
+#     else:
+#       print('Invalid key')
+#   except KeyboardInterrupt:
+#     break
