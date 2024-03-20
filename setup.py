ebd4efcc510de7fa0ec228a22e756470aa6882e2
Ibo <contact@iboibelings.nl>
2024-03-20 14:21:25 +0100

    Commented out unnecessary code in setup.py

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ def resetSequence():
   toPlotter('$1=255')
   time.sleep(1)
 
-  toPlotter('G92x0y0')
-  toPlotter('g1f1000')
-  toPlotter('z5')
-  time.sleep(2)
+  # toPlotter('G92x0y0')
+  # toPlotter('g1f1000')
+  # toPlotter('z5')
+  # time.sleep(2)
 
-  toPlotter('z0f1000')
-  time.sleep(1)
+  # toPlotter('z0f1000')
+  # time.sleep(1)
 
   toPlotter('$1=0')
   time.sleep(1)
