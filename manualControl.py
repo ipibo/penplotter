fd9a433762593a9b29fc1bedd03f4c3568454037
Ibo <contact@iboibelings.nl>
2024-03-20 13:55:48 +0100

    Refactor import statement in manualControl.py

--- manualControl.py
+++ manualControl.py
@@ -1,3 +1,21 @@
-from resetPlotter import resetSequence
+from setup import resetSequence
+from setup import resetSequence
+import keyboard
 
 resetSequence()
+
+while True:
+  try:
+    key = keyboard.read_key()
+    if key == 'up':
+      print('up')
+    elif key == 'down':
+      print('down')
+    elif key == 'left':
+      print('left')
+    elif key == 'right':
+      print('right')
+    else:
+      print('Invalid key')
+  except KeyboardInterrupt:
+    break
