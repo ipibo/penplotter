1e3c8ec62e278abb09d3c58d513841455624c696
Ibo <contact@iboibelings.nl>
2024-03-20 14:47:20 +0100

    go back to start on keybaord interupt

--- manualControl.py
+++ manualControl.py
@@ -1,5 +1,8 @@
 from setup import resetSequence
 from setup import toPlotter
+import os
+
+
 resetSequence()
 
 
@@ -11,24 +14,32 @@ def setSpeed(speed):
   toPlotter("f{0}".format(speed))
 
 def print_user_input():
-  while True:
-    xCoord = input("enter a x coordinate:")
-    print(xCoord)
-    yCoord = input("enter a y coordinate:")
-    print(yCoord)
-    # speed = input("enter a speed:")
-    # print(speed)
+  try:
+    while True:
+      xCoord = input("enter a x coordinate:")
+      print(xCoord)
+      yCoord = input("enter a y coordinate:")
+      print(yCoord)
+      
+      coord = "x{0}y{1}f{2}\n".format(xCoord, yCoord, speed)
+      coord = str(coord)
+      toPlotter(coord)
+
+  except:
+    print("error")
+    xCoord = 10
+    yCoord = 10
+    speed = 1000
     coord = "x{0}y{1}f{2}\n".format(xCoord, yCoord, speed)
     coord = str(coord)
     toPlotter(coord)
-    print(coord)
+
+
   
-
-
 # ask user for speed
 speed = input("enter a speed:")
-
 setSpeed(speed)
+
 
 
 print_user_input()
