1e5fde69006fafb2bc4c66c116e079dc957b78f8
Ibo <contact@iboibelings.nl>
2024-03-20 14:48:39 +0100

    Refactor print_user_input function to remove unnecessary try-except block

--- manualControl.py
+++ manualControl.py
@@ -14,25 +14,18 @@ def setSpeed(speed):
   toPlotter("f{0}".format(speed))
 
 def print_user_input():
-  try:
-    while True:
-      xCoord = input("enter a x coordinate:")
-      print(xCoord)
-      yCoord = input("enter a y coordinate:")
-      print(yCoord)
-      
-      coord = "x{0}y{1}f{2}\n".format(xCoord, yCoord, speed)
-      coord = str(coord)
-      toPlotter(coord)
-
-  except:
-    print("error")
-    xCoord = 10
-    yCoord = 10
-    speed = 1000
+  while True:
+    xCoord = input("enter a x coordinate:")
+    print(xCoord)
+    yCoord = input("enter a y coordinate:")
+    print(yCoord)
+    
     coord = "x{0}y{1}f{2}\n".format(xCoord, yCoord, speed)
     coord = str(coord)
     toPlotter(coord)
+    print(coord)
+
+
 
 
   
