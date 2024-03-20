9e72e338c3e9c715d5196b12d3eac2cb15d05a10
Ibo <contact@iboibelings.nl>
2024-03-20 14:10:35 +0100

    Add manual control functionality to print user input coordinates

--- manualControl.py
+++ manualControl.py
@@ -1,5 +1,5 @@
 from setup import resetSequence
-
+from setup import toPlotter
 # resetSequence()
 
 def on_key_press(event):
@@ -9,8 +9,19 @@ def on_key_press(event):
 
 
 def print_user_input():
-  user_input = input("Enter a string: ")
-  print(user_input)
+  xCoord = input("enter a x coordinate:")
+  print(xCoord)
+  yCoord = input("enter a y coordinate:")
+  print(yCoord)
+  speed = input("enter a speed:")
+  print(speed)
+  coord = "x{0}y{1}f{2}\n".format(xCoord, yCoord, speed)
+  coord = str(coord)
+  toPlotter(coord)
+  print(coord)
+  
+  
+
 
 
 print_user_input()
