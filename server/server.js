d13d4790ac18acdf47852d9be192960a8ff02180
Ibo <contact@iboibelings.nl>
2024-03-21 14:53:41 +0100

    Refactor code to improve performance and readability

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 const http = require("http")
+const { exec } = require("child_process")
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain")
-  res.end("Hello, world!")
+  if (req.url === "/run-script") {
+    // Execute the Python script
+    exec(
+      "python /home/pi/Desktop/penplotter/plotting/manualControl.py",
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing script: ${error}`)
+          res.statusCode = 500
+          res.end("Internal Server Error")
+        } else {
+          console.log(`Script output: ${stdout}`)
+          res.statusCode = 200
+          res.end("Script executed successfully")
+        }
+      }
+    )
+  } else {
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/plain")
+    res.end("Hello, world!")
+  }
 })
 
 const port = 3000
